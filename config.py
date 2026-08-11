df03935680c5ae19feb0c21e5bfdf9e71cb3aa67
iazzouzi <azzouziibrahim22@gmail.com>
2026-08-12 00:57:51 +0100

    add a config base class for game config params

---
+++ config.py
@@ -0,0 +1,13 @@
+class Config:
+
+    highscore_filename: str
+
+    level: list[tuple[int, int]]
+
+    points_per_pacgum: int
+
+    points_per_super_pacgum: int
+
+    points_per_ghost: int
+
+    level_max_time: int

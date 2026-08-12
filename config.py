97021b6fca4b6b22fc3ae615269475a40a8aef51
iazzouzi <azzouziibrahim22@gmail.com>
2026-08-12 16:03:18 +0100

    fix config class

--- config.py
+++ config.py
@@ -1,13 +1,18 @@
 class Config:
+    def __init__(self, highscore_filename: str, level:list[list[int]],
+                 lives: int, points_per_pacgum: int, points_per_super_pacgum: int,
+                 points_per_ghost: int, level_max_time:int):
 
-    highscore_filename: str
+        self.highscore_filename = highscore_filename
 
-    level: list[tuple[int, int]]
+        self.level = level
 
-    points_per_pacgum: int
+        self.lives = lives
 
-    points_per_super_pacgum: int
+        self.points_per_pacgum = points_per_pacgum
 
-    points_per_ghost: int
+        self.points_per_super_pacgum = points_per_super_pacgum
 
-    level_max_time: int
+        self.points_per_ghost = points_per_ghost
+
+        self.level_max_time = level_max_time

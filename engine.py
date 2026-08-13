a476bda00ac04c7f00b3fcffcfbbf3c1ad6a1d10
iazzouzi <azzouziibrahim22@gmail.com>
2026-08-13 14:50:04 +0100

    init the engine by config params

--- engine.py
+++ engine.py
@@ -1,4 +1,21 @@
 from mazegenerator import MazeGenerator
+from config import Config
 
 class Engine:
-    pass
+    def __init__(self, config: Config):
+
+        self.highscore_filename = config.highscore_filename
+
+        self.level = config.level
+
+        self.lives = config.lives
+
+        self.points_per_pacgum = config.points_per_pacgum
+
+        self.points_per_super_pacgum = config.points_per_super_pacgum
+
+        self.points_per_ghost = config.points_per_ghost
+
+        self.level_max_time = config.level_max_time
+
+    

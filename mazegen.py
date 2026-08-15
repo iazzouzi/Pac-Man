e6e041a2a679e9d7253bf39cad2ba8e77ac742eb
iazzouzi <azzouziibrahim22@gmail.com>
2026-08-15 22:20:45 +0100

    remove pacgums from the pattern cells

--- mazegen.py
+++ mazegen.py
@@ -5,7 +5,7 @@ from models import Ghost
 class MazeGen(MazeGenerator):
     def __init__(self, size = (15, 15), perfect = False, entry_cell = (0, 0), exit_cell = (-1, -1), seed = 0):
         super().__init__(size, perfect, entry_cell, exit_cell, seed)
-        self.pacgums_nb = size[0] * size[1] - 1
+        self.pacgums_nb = 0
         self.pacgums: list[Pacgum]
         self.pacgumsInit()
         self.ghosts: list[Ghost]
@@ -16,6 +16,8 @@ class MazeGen(MazeGenerator):
         self.pacgums = []
         for y in range(self._height):
             for x in range(self._width):
+                if self.maze[y][x] == 15:
+                    continue
                 if y == 0 and x == 0:
                     self.pacgums.append(Pacgum(x, y, super=True))
                 elif y == 0 and x == self._width - 1:
@@ -26,6 +28,7 @@ class MazeGen(MazeGenerator):
                     self.pacgums.append(Pacgum(x, y, super=True))
                 elif y != self._height / 2 and x != self._width / 2:
                     self.pacgums.append(Pacgum(x, y))
+                self.pacgums_nb += 1
 
     def ghostsInit(self):
         self.ghosts = []
